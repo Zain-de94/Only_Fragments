f1ba4ab550d16c52273b01f22801828e7f349b36
ZAIN <zainfarhan94@gmail.com>
2020-12-23 17:17:08 +0530

    Code after deleting actvities

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.android.miwok;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.awt.font.TextAttribute;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        CategoryAdapter adapter = new CategoryAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+
+    }
+
+
+}
+
+
+
